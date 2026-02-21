c757e6466084f723557aa28c13575a6a1b0157b8
Atilist <71230342+Atilist@users.noreply.github.com>
2026-02-21 23:12:55 +0100

    Diagonal Deviation is a Hypotenuse

--- src/main/java/com/gtnewhorizons/galaxia/dimension/WorldChunkManagerSpace.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/WorldChunkManagerSpace.java
@@ -100,7 +100,7 @@ public class WorldChunkManagerSpace extends WorldChunkManager {
     public double[] getAdjacentBiomeSignificance() {
         double xDeviation = Math.max(0, cacheNoiseX - cacheBiomeIndexX - 0.75) * 4;
         double zDeviation = Math.max(0, cacheNoiseZ - cacheBiomeIndexZ - 0.75) * 4;
-        double diagonalDeviation = xDeviation + zDeviation;
+        double diagonalDeviation = Math.sqrt(xDeviation*xDeviation + zDeviation*zDeviation);
         return new double[] {xDeviation, zDeviation, diagonalDeviation};
     }
 
