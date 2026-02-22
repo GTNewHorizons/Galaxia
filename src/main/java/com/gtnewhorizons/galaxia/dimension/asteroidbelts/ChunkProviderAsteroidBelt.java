0b7e03b51ecb4dc8f1614bb968c6ac284bcc583f
Atilist <71230342+Atilist@users.noreply.github.com>
2026-02-22 19:39:22 +0100

    Make ChunkProviderAsteroidBelt more legible and remove unused Code

--- src/main/java/com/gtnewhorizons/galaxia/dimension/asteroidbelts/ChunkProviderAsteroidBelt.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/asteroidbelts/ChunkProviderAsteroidBelt.java
@@ -3,7 +3,6 @@ package com.gtnewhorizons.galaxia.dimension.asteroidbelts;
 import java.util.List;
 import java.util.Random;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.IProgressUpdate;
 import net.minecraft.world.ChunkPosition;
@@ -11,7 +10,6 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.structure.MapGenScatteredFeature;
 
 import com.gtnewhorizons.galaxia.worldgen.Asteroid;
 
@@ -19,7 +17,6 @@ public class ChunkProviderAsteroidBelt implements IChunkProvider {
 
     private final Random rand;
     private final World worldObj;
-    private final MapGenScatteredFeature scatteredFeatureGenerator = new MapGenScatteredFeature();
     private final Asteroid[] asteroids;
 
     public ChunkProviderAsteroidBelt(World world, long seed, Asteroid[] asteroids) {
@@ -29,24 +26,19 @@ public class ChunkProviderAsteroidBelt implements IChunkProvider {
     }
 
     @Override
-    public boolean chunkExists(int p_73149_1_, int p_73149_2_) {
+    public boolean chunkExists(int x, int z) {
         return true;
     }
 
-    public Chunk provideChunk(int p_73154_1_, int p_73154_2_) {
-        this.rand.setSeed((long) p_73154_1_ * 341873128712L + (long) p_73154_2_ * 132897987541L);
-        Block[] ablock = new Block[65536];
-        byte[] abyte = new byte[65536];
-
-        Chunk chunk = new Chunk(this.worldObj, ablock, abyte, p_73154_1_, p_73154_2_);
-
+    public Chunk provideChunk(int chunkX, int chunkZ) {
+        Chunk chunk = new Chunk(worldObj, chunkX, chunkZ);
         chunk.generateSkylightMap();
         return chunk;
     }
 
     @Override
-    public Chunk loadChunk(int p_73158_1_, int p_73158_2_) {
-        return this.provideChunk(p_73158_1_, p_73158_2_);
+    public Chunk loadChunk(int x, int z) {
+        return this.provideChunk(x, z);
     }
 
     @Override
@@ -63,7 +55,7 @@ public class ChunkProviderAsteroidBelt implements IChunkProvider {
     }
 
     @Override
-    public boolean saveChunks(boolean p_73151_1_, IProgressUpdate p_73151_2_) {
+    public boolean saveChunks(boolean all, IProgressUpdate progressUpdate) {
         return true;
     }
 
@@ -83,18 +75,13 @@ public class ChunkProviderAsteroidBelt implements IChunkProvider {
     }
 
     @Override
-    public List<BiomeGenBase.SpawnListEntry> getPossibleCreatures(EnumCreatureType p_73155_1_, int p_73155_2_,
-        int p_73155_3_, int p_73155_4_) {
-        BiomeGenBase biomegenbase = this.worldObj.getBiomeGenForCoords(p_73155_2_, p_73155_4_);
-        return p_73155_1_ == EnumCreatureType.monster
-            && this.scatteredFeatureGenerator.func_143030_a(p_73155_2_, p_73155_3_, p_73155_4_)
-                ? this.scatteredFeatureGenerator.getScatteredFeatureSpawnList()
-                : biomegenbase.getSpawnableList(p_73155_1_);
+    public List<BiomeGenBase.SpawnListEntry> getPossibleCreatures(EnumCreatureType enumCreatureType, int x, int y,
+        int z) {
+        return null;
     }
 
     @Override
-    public ChunkPosition func_147416_a(World p_147416_1_, String p_147416_2_, int p_147416_3_, int p_147416_4_,
-        int p_147416_5_) {
+    public ChunkPosition func_147416_a(World world, String structure, int x, int y, int z) {
         return null;
     }
 
@@ -104,7 +91,7 @@ public class ChunkProviderAsteroidBelt implements IChunkProvider {
     }
 
     @Override
-    public void recreateStructures(int p_82695_1_, int p_82695_2_) {
+    public void recreateStructures(int x, int z) {
 
     }
 
