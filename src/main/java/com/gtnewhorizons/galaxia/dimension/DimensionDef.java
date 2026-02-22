4d7cd7c64fdc402cc18e489674bf5238afe21b2f
SkorchedEU <hdgaming700@gmail.com>
2026-02-22 23:01:19 +0000

    DimensionDef

--- src/main/java/com/gtnewhorizons/galaxia/dimension/DimensionDef.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/DimensionDef.java
@@ -7,6 +7,9 @@ import net.minecraft.world.WorldProvider;
 
 import com.gtnewhorizons.galaxia.dimension.sky.CelestialBody;
 
+/**
+ * Class to hold characteristics of the dimension (effectively a posh dataclass)
+ */
 public final class DimensionDef {
 
     public final String name;
@@ -19,11 +22,26 @@ public final class DimensionDef {
     public final List<CelestialBody> celestialBodies;
     public final EffectDef effects;
 
-    // Used in Orbital Calculator
+    // Used in Orbital Calculator primarily
     public final double mass;
     public final double orbitalRadius;
     public final double radius;
 
+    /**
+     * Constructs a definition based on the given parameters
+     * @param name The dimension name (from ENUM)
+     * @param id The dimension ID (from ENUM)
+     * @param provider The world provider used
+     * @param keepLoaded Whether to keep the dimension loaded (True -> Loaded)
+     * @param gravity The gravity multiplier (1 = Overworld, 0.1 = 10% of Overworld)
+     * @param airResistance The air resistance on planet
+     * @param removeSpeedCancelation Whether to cancel speed over time (see method in builder)
+     * @param celestialBodies Celestial Bodies List for sky-box buildiong
+     * @param mass The mass of the planet (used in calculations)
+     * @param orbitalRadius The distance from the system centre to the planet (used in calculations)
+     * @param radius The radius of the planet (used in calculations)
+     * @param effects The EffectDef containing all effects for this planet
+     */
     DimensionDef(String name, int id, Class<? extends WorldProvider> provider, boolean keepLoaded, double gravity,
         double airResistance, boolean removeSpeedCancelation, List<CelestialBody> celestialBodies, double mass,
         double orbitalRadius, double radius, EffectDef effects) {
