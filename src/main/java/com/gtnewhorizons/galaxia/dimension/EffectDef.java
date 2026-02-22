fe9fe71aa0180011a7779c88d1709de2d5cca7e6
SkorchedEU <hdgaming700@gmail.com>
2026-02-22 23:10:26 +0000

    EffectDef

--- src/main/java/com/gtnewhorizons/galaxia/dimension/EffectDef.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/EffectDef.java
@@ -1,5 +1,8 @@
 package com.gtnewhorizons.galaxia.dimension;
 
+/**
+ * Class to hold effects of the dimension (effectively a posh dataclass)
+ */
 public class EffectDef {
 
     public final int baseTemp;
@@ -9,6 +12,15 @@ public class EffectDef {
     public final boolean spores;
     public final int pressure;
 
+    /**
+     * Constructs a definition based on the given parameters
+     * @param baseTemp The temperature of the planet
+     * @param withering Whether withering is enabled on the planet
+     * @param oxygenPercent The relative oxygen level of the planet
+     * @param radiation The relative radiation level of the planet
+     * @param spores Whether spores are enabled on the planet
+     * @param pressure The relative pressure on the planet
+     */
     public EffectDef(int baseTemp, boolean withering, int oxygenPercent, int radiation, boolean spores, int pressure) {
         this.baseTemp = baseTemp;
         this.withering = withering;
@@ -18,6 +30,9 @@ public class EffectDef {
         this.pressure = pressure;
     }
 
+    /**
+     * A default implementation of the fleshed out constructor, with overworld like stats
+     */
     public EffectDef() {
         this.baseTemp = 273;
         this.withering = false;
