30151bd7f78e28ed30919d988ac26172f1acb07c
SkorchedEU <hdgaming700@gmail.com>
2026-02-22 23:07:47 +0000

    EffectBuilder

--- src/main/java/com/gtnewhorizons/galaxia/dimension/EffectBuilder.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/EffectBuilder.java
@@ -1,16 +1,21 @@
 package com.gtnewhorizons.galaxia.dimension;
 
+/**
+ * Builder class to get a list of effects on each planet as required
+ */
 public class EffectBuilder {
 
-    private int baseTemp;
-    private boolean withering;
-    private int oxygenPercent;
-    private int radiation;
-    private boolean spores;
-    private int pressure;
+    private int baseTemp; // Base temperature of planet
+    private boolean withering; // Whether the planet has a withering effect
+    private int oxygenPercent; // Oxygen percent (relative to Overworld -> 1)
+    private int radiation; // The level of radiation relative to Overworld (OW -> 1)
+    private boolean spores; // Whether the planet has fungal spores in atmosphere
+    private int pressure; // Pressure on surface relative to Overworld (OW -> 1)
 
     /**
-     * @param baseTemp temperature in Kelvins
+     * Sets the base temperature of the dimension
+     * @param baseTemp The required temperature
+     * @return Configured builder
      */
     public EffectBuilder baseTemp(int baseTemp) {
         this.baseTemp = baseTemp;
@@ -18,7 +23,9 @@ public class EffectBuilder {
     }
 
     /**
-     * @param oxygenPercent oxygen amount where 100 is earth atmosphere
+     * Sets the oxygen level relative to the overworld (OW -> 1)
+     * @param oxygenPercent The required oxygen level
+     * @return Configured builder
      */
     public EffectBuilder oxygenPercent(int oxygenPercent) {
         this.oxygenPercent = oxygenPercent;
@@ -26,28 +33,49 @@ public class EffectBuilder {
     }
 
     /**
-     * @param pressure pressure in earth atmospheres
+     * Sets the atmospheric pressure on the planet relative to the overworld (OW -> 1)
+     * @param pressure The required pressure level
+     * @return Configured builder
      */
     public EffectBuilder pressure(int pressure) {
         this.pressure = pressure;
         return this;
     }
 
+    /**
+     * Sets the radiation level relative to the overworld (OW -> 1)
+     * @param radiation The required radiation level
+     * @return Configured builder
+     */
     public EffectBuilder radiation(int radiation) {
         this.radiation = radiation;
         return this;
     }
 
+    /**
+     * Sets whether the planet should have a withering effect (See Ambergris)
+     * @param withering Boolean : True => wither effect
+     * @return Configured builder
+     */
     public EffectBuilder withering(boolean withering) {
         this.withering = withering;
         return this;
     }
 
+    /**
+     * Sets whether the planet should have fungal spores in the atmosphere (See Mykelia)
+     * @param spores Boolean : True => spores effect
+     * @return Configured builder
+     */
     public EffectBuilder spores(boolean spores) {
         this.spores = spores;
         return this;
     }
 
+    /**
+     * Builds an Effect Definition based on current fields
+     * @return Configured EffectDef
+     */
     public EffectDef build() {
         return new EffectDef(baseTemp, withering, oxygenPercent, radiation, spores, pressure);
     }
