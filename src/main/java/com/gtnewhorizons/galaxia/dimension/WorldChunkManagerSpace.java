e25f9b30d1fdb49f0015ba85ff08ba8f2376bec6
Atilist <71230342+Atilist@users.noreply.github.com>
2026-02-22 19:19:01 +0100

    Spotless Apply

--- src/main/java/com/gtnewhorizons/galaxia/dimension/WorldChunkManagerSpace.java
+++ src/main/java/com/gtnewhorizons/galaxia/dimension/WorldChunkManagerSpace.java
@@ -1,10 +1,7 @@
 package com.gtnewhorizons.galaxia.dimension;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
-import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.WorldChunkManager;
 import net.minecraft.world.gen.NoiseGeneratorOctaves;
