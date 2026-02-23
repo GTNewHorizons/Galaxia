9769f2ee150e09b272c171c8842c3435d23962fa
SkorchedEU <hdgaming700@gmail.com>
2026-02-23 01:11:44 +0000

    PlanetAPI

--- src/main/java/com/gtnewhorizons/galaxia/utility/PlanetAPI.java
+++ src/main/java/com/gtnewhorizons/galaxia/utility/PlanetAPI.java
@@ -6,8 +6,16 @@ import com.gtnewhorizons.galaxia.dimension.DimensionDef;
 import com.gtnewhorizons.galaxia.dimension.EffectDef;
 import com.gtnewhorizons.galaxia.dimension.SolarSystemRegistry;
 
+/**
+ * API underpinning planetary mechanics
+ */
 public final class PlanetAPI {
 
+    /**
+     * Gets the gravity on the planet, or returns 1 if failed
+     * @param e The entity to check effects on
+     * @return Gravity on the entity, or 1 if failed
+     */
     public static double getGravity(Entity e) {
         if (e == null || e.worldObj == null) return 1.0;
         DimensionDef def = SolarSystemRegistry.getById(e.dimension);
@@ -16,6 +24,11 @@ public final class PlanetAPI {
         // for some cases clamping might be required
     }
 
+    /**
+     * Gets the effects on the planet, or returns defaults if failed
+     * @param e The entity to check effects on
+     * @return Effects on the entity, or defaults if failed
+     */
     public static EffectDef getEffects(Entity e) {
         if (e == null || e.worldObj == null) return new EffectDef();
         DimensionDef def = SolarSystemRegistry.getById(e.dimension);
@@ -23,6 +36,11 @@ public final class PlanetAPI {
         return def.effects;
     }
 
+    /**
+     * Gets the air resistance on the planet, or returns 1 if failed
+     * @param e The entity to check effects on
+     * @return Air resistance on the entity, or 1 if failed
+     */
     public static double getAirResistance(Entity e) {
         if (e == null || e.worldObj == null) return 1.0;
         DimensionDef def = SolarSystemRegistry.getById(e.dimension);
@@ -30,6 +48,11 @@ public final class PlanetAPI {
         return def.air_resistance;
     }
 
+    /**
+     * Gets whether speed is cancelled
+     * @param e The entity to check effects on
+     * @return Boolean : True => Speed cancellation enabled
+     */
     public static boolean cancelSpeed(Entity e) {
         if (e == null || e.worldObj == null) return false;
         DimensionDef def = SolarSystemRegistry.getById(e.dimension);
