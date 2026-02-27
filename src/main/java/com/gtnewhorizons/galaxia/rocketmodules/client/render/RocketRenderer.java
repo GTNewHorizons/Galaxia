50adec3a56c6d4f47d47853ae607b75e756acc02
VortexSo4 <kreker.maxim@mail.ru>
2026-02-27 03:55:11 +0300

    fix rocket entity rendering issues

--- src/main/java/com/gtnewhorizons/galaxia/rocketmodules/client/render/RocketRenderer.java
+++ src/main/java/com/gtnewhorizons/galaxia/rocketmodules/client/render/RocketRenderer.java
@@ -2,6 +2,7 @@ package com.gtnewhorizons.galaxia.rocketmodules.client.render;
 
 import static com.gtnewhorizons.galaxia.rocketmodules.ModuleRegistry.getModule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.client.renderer.entity.Render;
@@ -27,26 +28,114 @@ public class RocketRenderer extends Render {
         List<Integer> types = rocket.getModuleTypes();
         if (types.isEmpty()) return;
 
-        double yOff = 0.75;
-        GL11.glDisable(GL11.GL_CULL_FACE);
+        List<ModuleInfo> commandModules = new ArrayList<>();
+        List<ModuleInfo> storageModules = new ArrayList<>();
+        List<ModuleInfo> fuelTanks = new ArrayList<>();
+        List<ModuleInfo> engines = new ArrayList<>();
+
         for (int type : types) {
             ModuleInfo info = getModule(type);
-            if (info == null) {
-                yOff += 2.0;
-                continue;
+            if (info == null) continue;
+
+            switch (type) {
+                case 0 -> fuelTanks.add(info);
+                case 1 -> commandModules.add(info);
+                case 2 -> storageModules.add(info);
+                case 3 -> engines.add(info);
             }
+        }
 
-            bindTexture(info.texture());
+        double yOff = 0.75;
+        GL11.glDisable(GL11.GL_CULL_FACE);
 
-            GL11.glPushMatrix();
-            GL11.glTranslated(x, y + yOff, z);
-            info.model()
-                .renderAll();
-            GL11.glPopMatrix();
+        int totalTanks = fuelTanks.size();
+        int tankIndex = 0;
+        int tanksRemaining = totalTanks;
+        int engineIndex = 0;
 
+        if (totalTanks <= 2) {
+            double tierEngineHeight = 0;
+            if (engineIndex < engines.size()) {
+                ModuleInfo engine = engines.get(engineIndex++);
+                renderModule(engine, x, y + yOff, z, 0, 0);
+                tierEngineHeight = engine.height();
+            }
+            yOff += tierEngineHeight;
+            for (ModuleInfo info : fuelTanks) {
+                renderModule(info, x, y + yOff, z, 0, 0);
+                yOff += info.height();
+            }
+        } else {
+            while (tanksRemaining > 0) {
+                int orbitalCount = Math.min(tanksRemaining - 1, 6);
+
+                double orbitRadius;
+                if (orbitalCount > 0) {
+                    double clearCentre = (fuelTanks.get(tankIndex)
+                        .width() / 2.0)
+                        + (fuelTanks.get(tankIndex + 1)
+                            .width() / 2.0);
+                    double clearEachOther = orbitalCount > 1 ? (fuelTanks.get(tankIndex + 1)
+                        .width()) / (2.0 * Math.sin(Math.PI / orbitalCount)) : 0;
+                    orbitRadius = Math.max(clearCentre, clearEachOther) + 0.1;
+                } else {
+                    orbitRadius = 0;
+                }
+
+                double tierEngineHeight = 0;
+                if (engineIndex < engines.size()) {
+                    ModuleInfo engine = engines.get(engineIndex++);
+                    renderModule(engine, x, y + yOff, z, 0, 0);
+                    tierEngineHeight = engine.height();
+                }
+
+                ModuleInfo centreTank = fuelTanks.get(tankIndex);
+                renderModule(centreTank, x, y + yOff + tierEngineHeight, z, 0, 0);
+                tankIndex++;
+                tanksRemaining--;
+
+                for (int o = 0; o < orbitalCount; o++) {
+                    double angle = (2 * Math.PI / orbitalCount) * o;
+                    double offsetX = Math.cos(angle) * orbitRadius;
+                    double offsetZ = Math.sin(angle) * orbitRadius;
+
+                    double orbitalEngineHeight = 0;
+                    if (engineIndex < engines.size()) {
+                        ModuleInfo engine = engines.get(engineIndex++);
+                        renderModule(engine, x, y + yOff, z, offsetX, offsetZ);
+                        orbitalEngineHeight = engine.height();
+                    }
+
+                    ModuleInfo orbitalTank = fuelTanks.get(tankIndex);
+                    renderModule(orbitalTank, x, y + yOff + orbitalEngineHeight, z, offsetX, offsetZ);
+                    tankIndex++;
+                    tanksRemaining--;
+                }
+
+                yOff += tierEngineHeight + centreTank.height();
+            }
+        }
+
+        for (ModuleInfo info : storageModules) {
+            renderModule(info, x, y + yOff, z, 0, 0);
             yOff += info.height();
         }
+
+        for (ModuleInfo info : commandModules) {
+            renderModule(info, x, y + yOff, z, 0, 0);
+            yOff += info.height();
+        }
+
         GL11.glEnable(GL11.GL_CULL_FACE);
+    }
+
+    private void renderModule(ModuleInfo info, double x, double y, double z, double offsetX, double offsetZ) {
+        bindTexture(info.texture());
+        GL11.glPushMatrix();
+        GL11.glTranslated(x + offsetX, y, z + offsetZ);
+        info.model()
+            .renderAll();
+        GL11.glPopMatrix();
     }
 
     @Override
--- src/main/java/com/gtnewhorizons/galaxia/rocketmodules/entities/EntityRocket.java
+++ src/main/java/com/gtnewhorizons/galaxia/rocketmodules/entities/EntityRocket.java
@@ -81,13 +81,115 @@ public class EntityRocket extends Entity {
         return new ArrayList<>(modules);
     }
 
-    private double getTotalHeight() {
+    private double computeVisualFuelAndEngineHeight() {
+        List<ModuleInfo> fuelTanks = new ArrayList<>();
+        List<ModuleInfo> engines = new ArrayList<>();
+        List<Integer> types = getModuleTypes();
+
+        for (int type : types) {
+            ModuleInfo info = ModuleRegistry.getModule(type);
+            if (info == null) continue;
+            if (type == 0) fuelTanks.add(info);
+            else if (type == 3) engines.add(info);
+        }
+
+        double yOff = 0.0;
+        int totalTanks = fuelTanks.size();
+        int tankIndex = 0;
+        int tanksRemaining = totalTanks;
+        int engineIndex = 0;
+
+        if (totalTanks <= 2) {
+            double tierEngineHeight = 0;
+            if (engineIndex < engines.size()) {
+                ModuleInfo engine = engines.get(engineIndex++);
+                tierEngineHeight = engine.height();
+            }
+            yOff += tierEngineHeight;
+            for (ModuleInfo info : fuelTanks) {
+                yOff += info.height();
+            }
+        } else {
+            while (tanksRemaining > 0) {
+                int orbitalCount = Math.min(tanksRemaining - 1, 6);
+
+                double tierEngineHeight = 0;
+                if (engineIndex < engines.size()) {
+                    ModuleInfo engine = engines.get(engineIndex++);
+                    tierEngineHeight = engine.height();
+                }
+
+                ModuleInfo centreTank = fuelTanks.get(tankIndex);
+                double centreHeight = centreTank.height();
+                tankIndex++;
+                tanksRemaining--;
+
+                for (int o = 0; o < orbitalCount; o++) {
+                    if (engineIndex < engines.size()) {
+                        engineIndex++;
+                    }
+                    tankIndex++;
+                    tanksRemaining--;
+                }
+
+                yOff += tierEngineHeight + centreHeight;
+            }
+        }
+        return yOff;
+    }
+
+    private double computeVisualStorageHeight() {
         double h = 0.0;
-        for (int t : modules) {
-            ModuleInfo info = ModuleRegistry.getModule(t);
-            h += info != null ? info.height() : 2.0;
+        for (int type : getModuleTypes()) {
+            if (type == 2) {
+                ModuleInfo info = ModuleRegistry.getModule(type);
+                if (info != null) h += info.height();
+            }
         }
         return h;
+    }
+
+    private double computeVisualCommandHeight() {
+        double h = 0.0;
+        for (int type : getModuleTypes()) {
+            if (type == 1) {
+                ModuleInfo info = ModuleRegistry.getModule(type);
+                if (info != null) h += info.height();
+            }
+        }
+        return h;
+    }
+
+    private double getTotalHeight() {
+        return computeVisualFuelAndEngineHeight() + computeVisualStorageHeight() + computeVisualCommandHeight();
+    }
+
+    @Override
+    public double getMountedYOffset() {
+        int cIdx = getCapsuleIndex();
+        if (cIdx < 0) return this.height * 0.75D;
+
+        List<Integer> types = getModuleTypes();
+        if (cIdx >= types.size()) return this.height * 0.75D;
+
+        ModuleInfo capInfo = ModuleRegistry.getModule(types.get(cIdx));
+        double capH = capInfo != null ? capInfo.height() : 2.5;
+
+        double fuelEngHeight = computeVisualFuelAndEngineHeight();
+        double storageHeight = computeVisualStorageHeight();
+
+        double commandsBeforeHeight = 0.0;
+        for (int i = 0; i < cIdx; i++) {
+            if (types.get(i) == 1) {
+                ModuleInfo info = ModuleRegistry.getModule(types.get(i));
+                if (info != null) {
+                    commandsBeforeHeight += info.height();
+                }
+            }
+        }
+
+        double capStartY = fuelEngHeight + storageHeight + commandsBeforeHeight;
+        return capStartY + capH * 0.4D;
     }
 
     @Override
@@ -108,23 +210,6 @@ public class EntityRocket extends Entity {
         if (Math.abs(this.height - newH) > 0.05F) {
             this.setSize(3.0F, newH);
         }
-    }
-
-    @Override
-    public double getMountedYOffset() {
-        int cIdx = getCapsuleIndex();
-        if (cIdx < 0) return this.height * 0.75D;
-
-        List<Integer> types = getModuleTypes();
-        double offset = 0.0;
-        for (int i = 0; i < cIdx && i < types.size(); i++) {
-            ModuleInfo info = ModuleRegistry.getModule(types.get(i));
-            offset += info != null ? info.height() : 2.0;
-        }
-
-        ModuleInfo capInfo = ModuleRegistry.getModule(types.get(cIdx));
-        double capH = capInfo != null ? capInfo.height() : 2.5;
-        return offset + capH * 0.4D;
     }
 
     @Override
