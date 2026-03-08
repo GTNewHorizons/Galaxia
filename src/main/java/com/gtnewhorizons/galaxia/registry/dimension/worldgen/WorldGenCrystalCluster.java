a84289c796275f72fd6eeaee1c30b298c01fa0cf
Atilist <71230342+Atilist@users.noreply.github.com>
2026-03-08 22:49:02 +0100

    Crystal Size Variation

--- src/main/java/com/gtnewhorizons/galaxia/registry/dimension/worldgen/WorldGenCrystalCluster.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/dimension/worldgen/WorldGenCrystalCluster.java
@@ -18,7 +18,7 @@ public class WorldGenCrystalCluster extends WorldGenGalaxiaCave {
         if (!super.generate(world, random, x, y, z)) {
             return false;
         }
-        for (int crystalCount = 0; crystalCount < random.nextInt(8) + 1; crystalCount++) {
+        for (int crystalCount = 0; crystalCount < random.nextInt(12) + 1; crystalCount++) {
             generateCrystal(world, random, x, y, z);
         }
         return true;
@@ -27,25 +27,39 @@ public class WorldGenCrystalCluster extends WorldGenGalaxiaCave {
     private void generateCrystal(World world, Random random, int x, int y, int z) {
         int height = random.nextInt(16) + 4;
         int straightness = random.nextInt(4) + 1;
-        int straightnessIterator = 0;
+        int straightnessIterator;
         int xTilt = random.nextInt(3) - 1;
         int zTilt = random.nextInt(3) - 1;
-        int xOffset = 0;
-        int zOffset = 0;
-        for (int yOffset = 0; yOffset < height; yOffset++) {
-            straightnessIterator++;
-            int combinedX = x + xOffset;
-            int combinedY = y + yOffset;
-            int combinedZ = z + zOffset;
-            if (straightnessIterator >= straightness) {
-                xOffset += xTilt;
-                zOffset += zTilt;
+        int xOffset;
+        int zOffset;
+        int thickness = 0;
+        if (random.nextInt(4) == 0) {
+            thickness++;
+        }
+        if (random.nextInt(8) == 0) {
+            thickness++;
+        }
+        for (int xThickness = -thickness; xThickness <= thickness; xThickness++) {
+            for (int zThickness = -thickness; zThickness <= thickness; zThickness++) {
                 straightnessIterator = 0;
+                xOffset = 0;
+                zOffset = 0;
+                for (int yOffset = 0; yOffset < height; yOffset++) {
+                    straightnessIterator++;
+                    int combinedX = x + xOffset + xThickness;
+                    int combinedY = y + yOffset;
+                    int combinedZ = z + zOffset + zThickness;
+                    if (straightnessIterator >= straightness) {
+                        xOffset += xTilt;
+                        zOffset += zTilt;
+                        straightnessIterator = 0;
+                    }
+                    if (!world.isAirBlock(combinedX, combinedY, combinedZ) && world.getBlock(combinedX, combinedY, combinedZ) != crystalBlock) {
+                        break;
+                    }
+                    setBlockFast(world, combinedX, combinedY, combinedZ, crystalBlock, 0);
+                }
             }
-            if (!world.isAirBlock(combinedX, combinedY, combinedZ) && world.getBlock(combinedX, combinedY, combinedZ) != crystalBlock) {
-                return;
-            }
-            setBlockFast(world, combinedX, combinedY, combinedZ, crystalBlock, 0);
         }
     }
 }
