1d63d44d4c6dfdd80f5e14ebf5d3665d78d19915
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-03-23 13:42:29 +0100

    Localized Dim Names, death messages, effects(+textures), fixes baubles textures

--- src/main/java/com/gtnewhorizons/galaxia/utility/effects/EffectLowOxygen.java
+++ src/main/java/com/gtnewhorizons/galaxia/utility/effects/EffectLowOxygen.java
@@ -1,23 +1,10 @@
 package com.gtnewhorizons.galaxia.utility.effects;
 
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-
 import com.gtnewhorizons.galaxia.utility.EnumColors;
 
-public class EffectLowOxygen extends Potion {
+public class EffectLowOxygen extends GalaxiaPotionEffect {
 
     public EffectLowOxygen(int id) {
-        super(id, true, EnumColors.EffectBad.getColor()); // isBadEffect=true
-        setPotionName("galaxia.effect.low_oxygen");
-        setIconIndex(0, 0);
-    }
-
-    @Override
-    public void performEffect(EntityLivingBase entity, int amplifier) {}
-
-    @Override
-    public boolean isReady(int duration, int amplifier) {
-        return true;
+        super(id, true, EnumColors.EffectBad.getColor(), "galaxia.effect.low_oxygen", 0, 0);
     }
 }
--- src/main/java/com/gtnewhorizons/galaxia/utility/effects/EffectOvercooling.java
+++ src/main/java/com/gtnewhorizons/galaxia/utility/effects/EffectOvercooling.java
@@ -1,23 +1,10 @@
 package com.gtnewhorizons.galaxia.utility.effects;
 
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-
 import com.gtnewhorizons.galaxia.utility.EnumColors;
 
-public class EffectOvercooling extends Potion {
+public class EffectOvercooling extends GalaxiaPotionEffect {
 
     public EffectOvercooling(int id) {
-        super(id, true, EnumColors.EffectBad.getColor()); // isBadEffect=true
-        setPotionName("galaxia.effect.overcooling");
-        setIconIndex(0, 0);
-    }
-
-    @Override
-    public void performEffect(EntityLivingBase entity, int amplifier) {}
-
-    @Override
-    public boolean isReady(int duration, int amplifier) {
-        return true;
+        super(id, true, EnumColors.EffectBad.getColor(), "galaxia.effect.overcooling", 2, 0);
     }
 }
