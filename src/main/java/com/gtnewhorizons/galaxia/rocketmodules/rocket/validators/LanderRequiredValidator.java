acf3f78a643531022ad7b4b25d04565b8380c97f
Stan Brown <hdgaming700@gmail.com>
2026-03-25 20:29:12 +0000

    Localised lander validator

--- src/main/java/com/gtnewhorizons/galaxia/rocketmodules/rocket/validators/LanderRequiredValidator.java
+++ src/main/java/com/gtnewhorizons/galaxia/rocketmodules/rocket/validators/LanderRequiredValidator.java
@@ -1,5 +1,7 @@
 package com.gtnewhorizons.galaxia.rocketmodules.rocket.validators;
 
+import net.minecraft.util.StatCollector;
+
 import com.gtnewhorizons.galaxia.rocketmodules.rocket.RocketAssembly;
 import com.gtnewhorizons.galaxia.rocketmodules.rocket.modules.LanderModule;
 
@@ -10,6 +12,9 @@ public class LanderRequiredValidator implements IRocketValidator {
         boolean hasCapsule = assembly.getModules()
             .stream()
             .anyMatch(m -> m instanceof LanderModule);
-        return hasCapsule ? ValidationResult.success() : new ValidationResult(false, "Requires a Lander module");
+        return hasCapsule ? ValidationResult.success()
+            : new ValidationResult(
+                false,
+                StatCollector.translateToLocal("galaxia.gui.rocket_silo.validator.lander_none"));
     }
 }
