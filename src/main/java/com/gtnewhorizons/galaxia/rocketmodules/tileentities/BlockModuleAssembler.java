c291db0251141216dfee812c72b7bececd0eb736
Meech <dewest88@gmail.com>
2026-04-05 01:12:23 -0400

    Refactor controller block logic into superclass

--- src/main/java/com/gtnewhorizons/galaxia/rocketmodules/tileentities/BlockModuleAssembler.java
+++ src/main/java/com/gtnewhorizons/galaxia/rocketmodules/tileentities/BlockModuleAssembler.java
@@ -1,85 +1,23 @@
 package com.gtnewhorizons.galaxia.rocketmodules.tileentities;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
 
 import com.cleanroommc.modularui.factory.GuiFactories;
 import com.gtnewhorizons.galaxia.registry.block.GalaxiaBlocksEnum;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
 /**
  * Block for the Module Assembler
  */
-public class BlockModuleAssembler extends Block implements ITileEntityProvider {
+public class BlockModuleAssembler extends BlockRocketController implements ITileEntityProvider {
 
-    @SideOnly(Side.CLIENT)
-    private IIcon frontIconOff;
-
-    @SideOnly(Side.CLIENT)
-    private IIcon frontIconOn;
-
-    /**
-     * Constructor class - sets material, textures etc.
-     */
     public BlockModuleAssembler() {
-        super(Material.rock);
-        this.setHardness(1.5F);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister iconRegister) {
-        this.frontIconOff = iconRegister.registerIcon("galaxia:machine/module_assembler_off");
-        this.frontIconOn = iconRegister.registerIcon("galaxia:machine/module_assembler_on");
-
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int meta) {
-        if (meta == 0) { // item in inventory - show the controller face
-            if (side == 4) return frontIconOff;
-            return GalaxiaBlocksEnum.RUSTY_PANEL.get()
-                .getIcon(side, 0);
-        }
-
-        // Metavalues 2-5 for when the structure is unformed, 6-9 for when it is formed
-        boolean formed = meta >= 6;
-        int facing = formed ? meta - 4 : meta;
-
-        if (side == facing) return formed ? frontIconOn : frontIconOff;
-        return GalaxiaBlocksEnum.RUSTY_PANEL.get()
-            .getIcon(side, 0);
-    }
-
-    /**
-     * Makes sure the controller face always faces the player
-     */
-    @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase placer, ItemStack stack) {
-
-        int facing = MathHelper.floor_double((placer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-
-        int meta = switch (facing) {
-            case 0 -> ForgeDirection.NORTH.ordinal(); // player faces south, front faces north
-            case 1 -> ForgeDirection.EAST.ordinal();
-            case 2 -> ForgeDirection.SOUTH.ordinal();
-            case 3 -> ForgeDirection.WEST.ordinal();
-            default -> ForgeDirection.NORTH.ordinal();
-        };
-        world.setBlockMetadataWithNotify(x, y, z, meta, 2);
+        super(
+            "galaxia:machine/module_assembler_on",
+            "galaxia:machine/module_assembler_off",
+            () -> GalaxiaBlocksEnum.RUSTY_PANEL.get()); // keep as lambda - ensures we load lazily
     }
 
     /**
