bc5fbd84855a50c78efc002a6d31883d7150d105
Pxx500 <pbartulik@gmail.com>
2026-04-21 23:22:39 +0200

    Update SystemAssetSort.java

--- src/main/java/com/gtnewhorizons/galaxia/client/gui/orbitalGUI/SystemAssetSort.java
+++ src/main/java/com/gtnewhorizons/galaxia/client/gui/orbitalGUI/SystemAssetSort.java
@@ -21,8 +21,7 @@ public enum SystemAssetSort {
                 .thenComparing(CelestialAsset::displayName);
             case BY_NAME -> Comparator.comparing(CelestialAsset::displayName);
             case BY_WARNINGS_FIRST -> Comparator.comparingInt(
-                (CelestialAsset a) -> -a.warningPriority()
-                    .ordinal())
+                (CelestialAsset a) -> -a.warningPriority().priority)
                 .thenComparing((CelestialAsset a) -> a.isInConstruction() ? 0 : 1)
                 .thenComparing(CelestialAsset::displayName);
         };
