0219b54ce56e4cef3a4bf24a7afcb550a2862824
Atilist <71230342+Atilist@users.noreply.github.com>
2026-05-07 22:22:52 +0200

    Multiple Tubes per Quadrant

--- src/main/java/com/gtnewhorizons/galaxia/registry/dimension/worldgen/noise/TubeNoise.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/dimension/worldgen/noise/TubeNoise.java
@@ -10,24 +10,26 @@ public class TubeNoise {
     private static final byte ADDITIONAL_BITSHIFT = 4;
     private static final byte TOTAL_BITSHIFT = CHUNK_BITSHIFT + ADDITIONAL_BITSHIFT;
     private static final short COORDINATE_BOUND = 2 << TOTAL_BITSHIFT;
-    private static final byte MINIMUM_TUBE_LENGTH = 100;
     private static final short DEVIATION_MARGIN = 16;
     private static final short SHIFT_MARGIN = 2 << (TOTAL_BITSHIFT - 1);
-    private static final byte HORIZONTAL_INCLINATION_MULTIPLIER = 16;
     private static final float VERTICAL_INCLINATION_MULTIPLIER = 0.5F;
 
     private final Random xRandom = new Random();
     private final Random zRandom = new Random();
-    private final LinearFunction3D linearFunction = new LinearFunction3D();
+    private final LinearFunction3D[] linearFunctions = new LinearFunction3D[8];
 
     private boolean cached = false;
     private long seed;
-    private int xStartPoint;
-    private int xEndPoint;
     private int cacheChunkX;
     private int cacheChunkZ;
     private int quadrantX;
     private int quadrantZ;
+
+    public TubeNoise() {
+        for (int i = 0; i < linearFunctions.length; i++) {
+            linearFunctions[i] = new LinearFunction3D();
+        }
+    }
 
     public boolean isCached() {
         return cached;
@@ -42,9 +44,12 @@ public class TubeNoise {
         z = Math.abs(z);
         x += quadrantX << ADDITIONAL_BITSHIFT;
         z += quadrantZ << ADDITIONAL_BITSHIFT;
-        if (x > xEndPoint) return false;
-        if (x < xStartPoint) return false;
-        return linearFunction.getDeviation(x, y, z) < DEVIATION_MARGIN;
+        for (LinearFunction3D linearFunction : linearFunctions) {
+            if (linearFunction.getDeviation(x, y, z) < DEVIATION_MARGIN) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean isInDifferentChunk(int chunkX, int chunkZ) {
@@ -61,27 +66,27 @@ public class TubeNoise {
         cached = true;
         xRandom.setSeed(seed + xQuadrant);
         zRandom.setSeed(seed + zQuadrant);
-        xEndPoint = xRandom.nextInt(COORDINATE_BOUND) + 1;
-        xStartPoint = xRandom.nextInt(Math.max(1, xEndPoint - MINIMUM_TUBE_LENGTH));
-        float zInclination = xRandom.nextFloat() * HORIZONTAL_INCLINATION_MULTIPLIER;
-        if (xRandom.nextBoolean()) {
-            zInclination = -zInclination;
+        for (LinearFunction3D linearFunction : linearFunctions) {
+            float zInclination = xRandom.nextFloat();
+            if (xRandom.nextBoolean()) {
+                zInclination = -zInclination;
+            }
+            float xyInclination = xRandom.nextFloat() * VERTICAL_INCLINATION_MULTIPLIER;
+            if (xRandom.nextBoolean()) {
+                xyInclination = -xyInclination;
+            }
+            float zyInclination = zRandom.nextFloat() * VERTICAL_INCLINATION_MULTIPLIER;
+            if (zRandom.nextBoolean()) {
+                zyInclination = -zyInclination;
+            }
+            linearFunction.setFunction(
+                zRandom.nextInt(COORDINATE_BOUND) - SHIFT_MARGIN,
+                xRandom.nextInt(ChunkProviderGalaxiaPlanet.HEIGHT_LIMIT >> 3),
+                zRandom.nextInt(ChunkProviderGalaxiaPlanet.HEIGHT_LIMIT >> 3),
+                zInclination,
+                xyInclination,
+                zyInclination
+            );
         }
-        float xyInclination = xRandom.nextFloat() * VERTICAL_INCLINATION_MULTIPLIER;
-        if (xRandom.nextBoolean()) {
-            xyInclination = -xyInclination;
-        }
-        float zyInclination = zRandom.nextFloat() * VERTICAL_INCLINATION_MULTIPLIER;
-        if (zRandom.nextBoolean()) {
-            zyInclination = -zyInclination;
-        }
-        linearFunction.setFunction(
-            zRandom.nextInt(COORDINATE_BOUND) - SHIFT_MARGIN,
-            xRandom.nextInt(ChunkProviderGalaxiaPlanet.HEIGHT_LIMIT >> 3),
-            zRandom.nextInt(ChunkProviderGalaxiaPlanet.HEIGHT_LIMIT >> 3),
-            zInclination,
-            xyInclination,
-            zyInclination
-        );
     }
 }
