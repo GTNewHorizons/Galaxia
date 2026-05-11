a419c226b91aab8d432753776062e4cd023f8204
VortexSo4 <kreker.maxim@mail.ru>
2026-05-11 07:51:01 +0300

    separate client and server packet registration to prevent class loading errors

--- src/main/java/com/gtnewhorizons/galaxia/core/network/BeamEffectPacketClientHandler.java
+++ src/main/java/com/gtnewhorizons/galaxia/core/network/BeamEffectPacketClientHandler.java
@@ -6,7 +6,10 @@ import net.minecraft.world.World;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class BeamEffectPacketClientHandler implements IMessageHandler<BeamEffectPacket, IMessage> {
 
     @Override
