927fd0fc10bee09b5fedfd8ba53a304d37b54c55
VortexSo4 <kreker.maxim@mail.ru>
2026-05-11 05:44:08 +0300

    somewhat works

--- src/main/java/com/gtnewhorizons/galaxia/registry/block/tile/machine/gui/LeafScanWidget.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/block/tile/machine/gui/LeafScanWidget.java
@@ -1,94 +1,55 @@
 package com.gtnewhorizons.galaxia.registry.block.tile.machine.gui;
 
+import com.cleanroommc.modularui.drawable.UITexture;
+import com.cleanroommc.modularui.theme.WidgetTheme;
+import com.gtnewhorizons.galaxia.client.EnumColors;
 import com.gtnewhorizons.galaxia.registry.block.tile.machine.TileEntityOxygenCollector;
 import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-
-import org.lwjgl.opengl.GL11;
-
-import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
 import com.cleanroommc.modularui.theme.WidgetThemeEntry;
 import com.cleanroommc.modularui.widget.Widget;
 import com.gtnewhorizons.galaxia.client.EnumTextures;
 
 public class LeafScanWidget extends Widget<LeafScanWidget> {
-
-    private static final int GRID_SIZE = 9;
-    private static final int CELL_SIZE = 9;
-    private static final int SCAN_RADIUS = 4;
-
     private final TileEntityOxygenCollector tile;
+    private static final UITexture GRID_TEX = EnumTextures.MIRAGE.getImage();
+    private static final UITexture DOT_TEX = EnumTextures.TENEBRAE.getImage();
 
     public LeafScanWidget(TileEntityOxygenCollector tile) {
         this.tile = tile;
-
+        size(81, 81);
         background(EnumTextures.SELECTION_FRAME.getImage());
-        tooltip(t -> t.addLine(IKey.lang("galaxia.gui.oxygen_filler.leaf_scan")));
     }
 
     @Override
     public void draw(ModularGuiContext context, WidgetThemeEntry<?> widgetTheme) {
         super.draw(context, widgetTheme);
 
-        if (tile.getWorldObj() == null) {
-            return;
-        }
+        GRID_TEX.draw(context, getArea(), WidgetTheme.getDefault().getTheme());
 
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-
-        for (int gridX = 0; gridX < GRID_SIZE; gridX++) {
-            for (int gridY = 0; gridY < GRID_SIZE; gridY++) {
-
-                int worldX = tile.xCoord + (gridX - SCAN_RADIUS);
-                int worldZ = tile.zCoord + (gridY - SCAN_RADIUS);
-
-                boolean foundLeaf = false;
-
-                for (int y = tile.yCoord - 2; y <= tile.yCoord + 2; y++) {
-                    Block block = tile.getWorldObj().getBlock(worldX, y, worldZ);
-
-                    if (block == Blocks.leaves || block == Blocks.leaves2) {
-                        foundLeaf = true;
-                        break;
-                    }
-
-                    if (block != null && block.isLeaves(tile.getWorldObj(), worldX, y, worldZ)) {
-                        foundLeaf = true;
-                        break;
-                    }
+        int radius = 4;
+        for (int dx = -radius; dx <= radius; dx++) {
+            for (int dz = -radius; dz <= radius; dz++) {
+                if (hasLeafAt(dx, dz)) {
+                    int px = (dx + radius) * 9;
+                    int py = (dz + radius) * 9;
+                    DOT_TEX.withColorOverride(EnumColors.MAP_LEAF_GREEN.getColor())
+                        .draw(context, px + 1, py + 1, 7, 7, widgetTheme.getTheme());
                 }
-
-                int px = gridX * CELL_SIZE;
-                int py = gridY * CELL_SIZE;
-
-                if (foundLeaf) {
-                    GL11.glColor4f(0.2f, 1.0f, 0.3f, 1.0f);
-                } else {
-                    GL11.glColor4f(0.25f, 0.25f, 0.25f, 1.0f);
-                }
-
-                GL11.glBegin(GL11.GL_QUADS);
-                GL11.glVertex2f(px + 1, py + 1);
-                GL11.glVertex2f(px + CELL_SIZE - 1, py + 1);
-                GL11.glVertex2f(px + CELL_SIZE - 1, py + CELL_SIZE - 1);
-                GL11.glVertex2f(px + 1, py + CELL_SIZE - 1);
-                GL11.glEnd();
             }
         }
+        DOT_TEX.withColorOverride(EnumColors.MAP_MACHINE_BLUE.getColor())
+            .draw(context, 37, 37, 7, 7, widgetTheme.getTheme());
+    }
 
-        int centerX = (GRID_SIZE / 2) * CELL_SIZE;
-        int centerY = (GRID_SIZE / 2) * CELL_SIZE;
-
-        GL11.glColor4f(0.3f, 0.9f, 1.0f, 1.0f);
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2f(centerX + 2, centerY + 2);
-        GL11.glVertex2f(centerX + CELL_SIZE - 2, centerY + 2);
-        GL11.glVertex2f(centerX + CELL_SIZE - 2, centerY + CELL_SIZE - 2);
-        GL11.glVertex2f(centerX + 2, centerY + CELL_SIZE - 2);
-        GL11.glEnd();
-
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glColor4f(1f, 1f, 1f, 1f);
+    private boolean hasLeafAt(int dx, int dz) {
+        if (tile.getWorldObj() == null) return false;
+        int worldX = tile.xCoord + dx;
+        int worldZ = tile.zCoord + dz;
+        for (int y = tile.yCoord - 3; y <= tile.yCoord + 3; y++) {
+            Block b = tile.getWorldObj().getBlock(worldX, y, worldZ);
+            if (b != null && b.isLeaves(tile.getWorldObj(), worldX, y, worldZ)) return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/gtnewhorizons/galaxia/registry/block/tile/machine/gui/OxygenFillerGUI.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/block/tile/machine/gui/OxygenFillerGUI.java
@@ -1,5 +1,7 @@
 package com.gtnewhorizons.galaxia.registry.block.tile.machine.gui;
 
+import com.cleanroommc.modularui.utils.Alignment;
+import com.gtnewhorizons.galaxia.client.EnumTextures;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 
@@ -18,67 +20,40 @@ import com.gtnewhorizons.galaxia.registry.block.tile.machine.TileEntityOxygenFil
 public class OxygenFillerGUI {
 
     public static ModularPanel build(TileEntityOxygenFiller tile, PosGuiData guiData, PanelSyncManager syncManager) {
-        IntSyncValue energySync = new IntSyncValue(() -> (int) Math.min(tile.storedEnergy, Integer.MAX_VALUE), v -> {});
+        IntSyncValue energySync = new IntSyncValue(() -> (int) Math.min(tile.storedEnergy, Integer.MAX_VALUE), _ -> {});
         IntSyncValue maxEnergySync = new IntSyncValue(
             () -> (int) Math.min(tile.getMaxEnergyBuffer(), Integer.MAX_VALUE),
-            v -> {});
-        IntSyncValue oxygenSync = new IntSyncValue(tile::getStoredOxygen, v -> {});
-        IntSyncValue maxOxygenSync = new IntSyncValue(tile::getMaxOxygenBuffer, v -> {});
+            _ -> {});
+        IntSyncValue oxygenSync = new IntSyncValue(tile::getStoredOxygen, _ -> {});
+        IntSyncValue maxOxygenSync = new IntSyncValue(tile::getMaxOxygenBuffer, _ -> {});
 
         syncManager.syncValue("energy", energySync);
         syncManager.syncValue("maxEnergy", maxEnergySync);
         syncManager.syncValue("oxygen", oxygenSync);
         syncManager.syncValue("maxOxygen", maxOxygenSync);
 
-        IItemHandler handler = tile.getItemHandler();
-
-        return ModularPanel.defaultPanel("oxygen_filler", 176, 240)
+        return ModularPanel.defaultPanel("oxygen_filler", 176, 220)
             .bindPlayerInventory()
-            .child(
-                IKey.lang("galaxia.gui.oxygen_filler.title")
-                    .color(EnumColors.MAP_COLOR_TEXT_TITLE.getColor())
-                    .asWidget()
-                    .top(6)
-                    .left(8))
-
-            // Bars
-            .child(GUIHelper.createEnergyBar(energySync, maxEnergySync, 8, 22))
-            .child(GUIHelper.createOxygenBar(oxygenSync, maxOxygenSync, 8, 48))
-
-            // Machine slots 3x2
-            .child(createMachineSlots(handler, 8, 160))
-
-            // Status
-            .child(
-                IKey.dynamic(
-                    () -> tile.active
-                        ? EnumChatFormatting.GREEN + StatCollector.translateToLocal("galaxia.gui.common.filling")
-                        : EnumChatFormatting.GRAY + StatCollector.translateToLocal("galaxia.gui.common.idle"))
-                    .asWidget()
-                    .top(222)
-                    .left(8));
+            .child(IKey.lang("galaxia.gui.oxygen_filler.title")
+                .color(EnumColors.MAP_COLOR_TEXT_TITLE.getColor())
+                .asWidget().top(6).left(8))
+            .child(GUIHelper.createEnergyBar(energySync, maxEnergySync, 8, 20))
+            .child(GUIHelper.createOxygenBar(oxygenSync, maxOxygenSync, 8, 35))
+            .child(createMachineSlots(tile.getItemHandler(), 0, 0).center())
+            .child(IKey.dynamic(() -> tile.active
+                    ? EnumChatFormatting.GREEN + StatCollector.translateToLocal("galaxia.gui.common.filling")
+                    : EnumChatFormatting.GRAY + StatCollector.translateToLocal("galaxia.gui.common.idle"))
+                .asWidget().bottom(90).left(8));
     }
 
     private static Flow createMachineSlots(IItemHandler handler, int x, int y) {
-        Flow row1 = Flow.row()
+        return Flow.row()
             .top(y)
-            .left(x);
-        Flow row2 = Flow.row()
-            .top(y + 20)
-            .left(x);
-
-        for (int i = 0; i < 3; i++) {
-            row1.child(
-                new ItemSlot().slot(new ModularSlot(handler, i))
-                    .marginRight(2));
-        }
-        for (int i = 3; i < 6; i++) {
-            row2.child(
-                new ItemSlot().slot(new ModularSlot(handler, i))
-                    .marginRight(2));
-        }
-        return Flow.column()
-            .child(row1)
-            .child(row2);
+            .left(x)
+            .childPadding(6)
+            .crossAxisAlignment(Alignment.CrossAxis.CENTER)
+            .child(new ItemSlot().slot(new ModularSlot(handler, 0)))
+            .child(new ItemSlot().slot(new ModularSlot(handler, 1)))
+            .coverChildren();
     }
 }
