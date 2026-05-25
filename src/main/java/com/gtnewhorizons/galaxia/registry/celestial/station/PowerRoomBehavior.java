8d8fe9e77675caceb294bb69c4a86363010be934
sbancuz <sbancuz@protonmail.com>
2026-05-25 20:10:16 +0200

    Power room should be enclosed

--- src/main/java/com/gtnewhorizons/galaxia/registry/celestial/station/PowerRoomBehavior.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/celestial/station/PowerRoomBehavior.java
@@ -64,8 +64,8 @@ public class PowerRoomBehavior implements IStationBehaviorWithAttachments {
                 return false;
             }, GalaxiaBlocksEnum.AIRLOCK_CONTROLLER.get(), 0))
             .embedDefinition(TileEntityAirlock.STRUCTURE_PIECE_MAIN, TileEntityAirlock.STRUCTURE_DEFINITION)
-            .withSearchRadius(ConfigStructures.open.searchRadius)
-            .open()
+            .withSearchRadius(ConfigStructures.enclosed.searchRadius)
+            .enclosed()
             .build();
     }
 
