99946e1640cf6c5893a51d137e78e44ce03ad518
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2026-05-29 22:04:27 -0600

    moon smooth blocks

--- src/main/java/com/gtnewhorizons/galaxia/registry/block/GalaxiaBlocksEnum.java
+++ src/main/java/com/gtnewhorizons/galaxia/registry/block/GalaxiaBlocksEnum.java
@@ -84,6 +84,11 @@ public enum GalaxiaBlocksEnum {
     MOON_TEKTITE_BRICK(new BlockConfigurable("moon/decoration/moon_tektite_bricks")),
     MOON_BRECCIA_BRICK(new BlockConfigurable("moon/decoration/moon_breccia_bricks")),
     MOON_GABBRO_BRICKS(new BlockConfigurable("moon/decoration/moon_gabbro_bricks")),
+    MOON_ANDESITE_SMOOTH(new BlockConfigurable("moon/decoration/moon_andesite_smooth")),
+    MOON_BASALT_SMOOTH(new BlockConfigurable("moon/decoration/moon_basalt_smooth")),
+    MOON_TEKTITE_SMOOTH(new BlockConfigurable("moon/decoration/moon_tektite_smooth")),
+    MOON_BRECCIA_SMOOTH(new BlockConfigurable("moon/decoration/moon_breccia_smooth")),
+    MOON_GABBRO_SMOOTH(new BlockConfigurable("moon/decoration/moon_gabbro_smooth")),
 
     // MISC - MULTIBLOCK BLOCKS
     RUSTY_SCAFFOLDING(new BlockCasing("rusty_scaffolding").transparent()),
