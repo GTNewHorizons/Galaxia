e872d39762d8f11631189b3e1e46b20e32642817
sawyergt <287409923+sawyergt@users.noreply.github.com>
2026-06-10 18:20:49 +0200

    move `EntityConstructionHandler` + register the handler

--- src/main/java/com/gtnewhorizons/galaxia/core/GalaxiaPlayerProperties.java
+++ src/main/java/com/gtnewhorizons/galaxia/core/GalaxiaPlayerProperties.java
@@ -7,7 +7,14 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * Holds properties tied to the player itself
+ */
 public class GalaxiaPlayerProperties implements IExtendedEntityProperties {
 
     public static final String propertyId = "GalaxiaPlayerProperties";
@@ -41,5 +48,43 @@ public class GalaxiaPlayerProperties implements IExtendedEntityProperties {
     // convenience method for getting the instance tied to a player
     public static GalaxiaPlayerProperties get(@Nonnull EntityPlayer p) {
         return (GalaxiaPlayerProperties) p.getExtendedProperties(propertyId);
+    }
+
+    /**
+     * Copies data that should persist between clones from `oldProperties` to `this`
+     *
+     * @param oldProperties properties to copy from
+     */
+    private void copyFrom(@Nonnull GalaxiaPlayerProperties oldProperties) {
+        this.lowOxygenDuration = oldProperties.lowOxygenDuration;
+    }
+
+    /**
+     * Called when the player has died, used to reset any properties that shouldn't persist between
+     * respawns.
+     */
+    private void onDeath() {
+        this.lowOxygenDuration = 0;
+    }
+
+    public final static class PlayerEventHandler {
+
+        @SubscribeEvent
+        public void onClonePlayer(PlayerEvent.Clone p) {
+            GalaxiaPlayerProperties oldData = GalaxiaPlayerProperties.get(p.original);
+            GalaxiaPlayerProperties newData = GalaxiaPlayerProperties.get(p.entityPlayer);
+
+            newData.copyFrom(oldData);
+
+            if (p.wasDeath) newData.onDeath();
+        }
+
+        @SubscribeEvent
+        public void entityConstruct(EntityEvent.EntityConstructing e) {
+            if (!(e.entity instanceof EntityPlayer)) return;
+            if (e.entity.getExtendedProperties(GalaxiaPlayerProperties.propertyId) != null) return;
+
+            e.entity.registerExtendedProperties(GalaxiaPlayerProperties.propertyId, new GalaxiaPlayerProperties());
+        }
     }
 }
