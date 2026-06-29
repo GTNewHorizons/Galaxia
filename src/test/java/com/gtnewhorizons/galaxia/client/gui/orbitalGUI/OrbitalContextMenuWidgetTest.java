3ca43f316952fbfb37bbaf2582a39f26575eaa2e
Pxx500 <pbartulik@gmail.com>
2026-06-29 21:43:11 +0200

    T39 Localize orbital satellite menu actions

---
+++ src/test/java/com/gtnewhorizons/galaxia/client/gui/orbitalGUI/OrbitalContextMenuWidgetTest.java
@@ -0,0 +1,41 @@
+package com.gtnewhorizons.galaxia.client.gui.orbitalGUI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.gtnewhorizons.galaxia.registry.celestial.CelestialObject;
+import com.gtnewhorizons.galaxia.registry.celestial.CelestialObjectId;
+import com.gtnewhorizons.galaxia.registry.celestial.CelestialRegistry;
+import com.gtnewhorizons.galaxia.testing.GalaxiaTestBootstrap;
+
+final class OrbitalContextMenuWidgetTest {
+
+    @BeforeAll
+    static void init() {
+        GalaxiaTestBootstrap.ensureCelestialRegistry();
+    }
+
+    @Test
+    void contextMenuActionsExposeLocalizationKeys() {
+        CelestialObject body = CelestialRegistry.get(CelestialObjectId.MARS)
+            .orElseThrow();
+
+        List<String> labelKeys = OrbitalContextMenuWidget.buildActions(body, true)
+            .stream()
+            .map(ContextMenuAction::labelKey)
+            .toList();
+
+        assertEquals(
+            List.of(
+                "galaxia.gui.orbital.context_menu.manage_assets",
+                "galaxia.satellite.action.add_communication",
+                "galaxia.satellite.action.delete_communication",
+                "galaxia.satellite.action.add_prospecting",
+                "galaxia.satellite.action.delete_prospecting"),
+            labelKeys);
+    }
+}
