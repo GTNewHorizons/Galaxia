ed97cde7ba0341bcb31e305f87080e1455462060
Pxx500 <pbartulik@gmail.com>
2026-07-01 12:03:26 +0200

    add station module placeholder textures

--- src/test/java/com/gtnewhorizons/galaxia/client/gui/station/layer/StationTextureRegistryTest.java
+++ src/test/java/com/gtnewhorizons/galaxia/client/gui/station/layer/StationTextureRegistryTest.java
@@ -1,6 +1,7 @@
 package com.gtnewhorizons.galaxia.client.gui.station.layer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import net.minecraft.util.ResourceLocation;
 
@@ -10,6 +11,19 @@ import com.gtnewhorizons.galaxia.client.gui.station.layer.StationTextureRegistry
 import com.gtnewhorizons.galaxia.registry.outpost.module.FacilityModuleKind;
 
 final class StationTextureRegistryTest {
+
+    @Test
+    void everyModuleTexturePathHasPackagedAsset() {
+        ClassLoader classLoader = StationTextureRegistryTest.class.getClassLoader();
+        for (FacilityModuleKind kind : FacilityModuleKind.values()) {
+            ResourceLocation texture = StationTextureRegistry.moduleTexture(kind);
+
+            assertNotNull(texture, "missing module texture path for " + kind);
+            assertNotNull(
+                classLoader.getResource("assets/" + texture.getResourceDomain() + "/" + texture.getResourcePath()),
+                "missing module texture asset for " + kind + ": " + texture);
+        }
+    }
 
     @Test
     void capacityConnectorTexturesAreNamedByModuleKindAndDirection() {
